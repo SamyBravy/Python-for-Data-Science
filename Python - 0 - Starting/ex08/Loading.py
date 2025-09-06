182066b4fd66c596e0e7f46fb7b353e56c62219c
SamyBravy <samybravy@gmail.com>
2025-09-06 12:37:25 +0200

    fine davvero

--- Python - 0 - Starting/ex08/Loading.py
+++ Python - 0 - Starting/ex08/Loading.py
@@ -3,18 +3,17 @@ def ft_tqdm(lst: range) -> None:
     if len(lst) == 0:
         return
 
+    bar_len = 102
     for i in range(len(lst)):
         part = i / len(lst)
         perc = round(part * 100)
         print(f"\r{perc:3d}%", end='')
 
-        bar_len = 102
         filled = round(part * bar_len)
         bar = '█' * filled + ' ' * (bar_len - filled)
         print(f"|{bar}", end='')
 
         print(f"| {i}/{len(lst)}", end='')
-        i += 1
-        yield lst[i - 1]
+        yield lst[i]
 
     print(f"\r100%|{'█' * 102}| {len(lst)}/{len(lst)}")
